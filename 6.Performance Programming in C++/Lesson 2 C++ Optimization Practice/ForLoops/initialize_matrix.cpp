4b78cd97c3e2f0ffe8e1d4541e96d303263d0ce3
jiangnanqw12 <41460494+jiangnanqw12@users.noreply.github.com>
2019-08-22 18:36:45 +0800

    addcode

--- 6.Performance Programming in C++/Lesson 2 C++ Optimization Practice/ForLoops/initialize_matrix.cpp
+++ 6.Performance Programming in C++/Lesson 2 C++ Optimization Practice/ForLoops/initialize_matrix.cpp
@@ -5,7 +5,18 @@ vector < vector<int> > initialize_matrix(int num_rows, int num_cols, int initial
     
     vector < vector<int> > matrix;
     vector<int> new_row;
+    /*
+    new_row.reserve(num_cols);
+    matrix.reserve(num_rows);
+    for (int j = 0; j < num_cols; j++) {
+        new_row.push_back(initial_value);
+    }
     
+    for (int i = 0; i < num_rows; i++) {
+        matrix.push_back(new_row);
+    }
+    */
+    //more efficient
     for (int i = 0; i < num_rows; i++) {
         new_row.clear();
         for (int j = 0; j < num_cols; j++) {
@@ -13,6 +24,6 @@ vector < vector<int> > initialize_matrix(int num_rows, int num_cols, int initial
         }
         matrix.push_back(new_row);
     }
-
+    
     return matrix;
 }
